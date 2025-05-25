fbab31d672eb957ba7d725664df840f3ab9de4da
linux3033 <lio3030albino@hotmail.com>
2025-05-25 14:39:33 -0400

    changes 6.0

--- models/Venta.js
+++
@@ -1,9 +0,0 @@
-class Venta {
-    constructor(cliente, nOrden, fecha, precio, estado) {
-        this.cliente = cliente;
-        this.nOrden = nOrden;
-        this.fecha = fecha = new Date(fecha);
-        this.precio = precio;
-        this.estado = estado;
-    }
-}
