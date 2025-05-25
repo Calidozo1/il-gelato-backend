24f4ab4b09e8b3f8c8d82ffd978e95ffd370b25f
linux3033 <lio3030albino@hotmail.com>
2025-05-24 20:32:15 -0400

    changes 3.0

--- models/Venta.js
+++ models/Venta.js
@@ -1,13 +1,9 @@
-export default class Venta {
+class Venta {
     constructor(cliente, nOrden, fecha, precio, estado) {
         this.cliente = cliente;
         this.nOrden = nOrden;
         this.fecha = fecha = new Date(fecha);
         this.precio = precio;
         this.estado = estado;
-    }
-
-    fechaConversion(){
-        return this.fecha.toLocaleDateString('es-ES');
     }
 }
