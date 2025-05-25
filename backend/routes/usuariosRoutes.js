b071fa0d9515639c85b1aab0ccd29c0c9c03d68b
Calidozo1 <acardozo164@gmail.com>
2025-05-25 00:26:51 -0400

    controlador de perfiles
    ya se puede registrar perfiles de manera local y tiene sus validaciones

--- backend/routes/usuariosRoutes.js
+++ backend/routes/usuariosRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { iniciarSesion } = require('../controllers/registroController');
-const { crearPerfil } = require('../controllers/registroController');
 
-router.post('/signup', (req, res) => {
+const { crearPerfil } = require('../controllers/controladorPerfil');
+
+router.post('/registrar', (req, res) => {
     const { nombre, email, cedula, password, phone } = req.body;
 
     const resultado = crearPerfil(nombre, email, cedula, password, phone);
@@ -11,17 +11,6 @@ router.post('/signup', (req, res) => {
         res.status(200).json(resultado);
     } else {
         res.status(400).json(resultado);
-    }
-});
-
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    const resultado = iniciarSesion(email, password);
-
-    if (resultado.exito) {
-        res.status(200).json(resultado);
-    } else {
-        res.status(401).json(resultado); // 401 = Unauthorized
     }
 });
 
