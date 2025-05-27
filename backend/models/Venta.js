95e38ae91d118dcab274e7b138b5a237927169a6
linux3033 <116753394+linux3033@users.noreply.github.com>
2025-05-26 22:48:57 -0400

    ELiminacion en el model consultar venta

--- backend/models/Venta.js
+++ backend/models/Venta.js
@@ -1,19 +1,16 @@
 class Venta {
-    constructor(cliente, nOrden,pedido,precio,fecha,hora,estado) {
+    constructor(cliente, nOrden,fecha,hora) {
         this.cliente = cliente;
         this.nOrden = nOrden;
-        this.pedido = pedido;
-        this.precio = precio;
         this.fecha = fecha;
         this.hora = hora;
-        this.estado = estado;
     }
 }
 
 const ventas = [
-    new Venta("Valentina Da Camara", "AA002", "Helado1",5.4,"2025-05-20",  "12:30:00","Pendiente"),
-    new Venta("Pedro Da Silva", "AA003", "Helado2", 2.7,"2025-06-21", "08:30:00","Entregado"),
-    new Venta("María González", "AA004", "Helado3",8.1,"2025-05-21","08:30:00" , "Entregado")
+    new Venta("Valentina Da Camara", "AA002","2025-05-20",  "12:30:00"),
+    new Venta("Pedro Da Silva", "AA003","2025-06-21", "08:30:00"),
+    new Venta("María González", "AA004","2025-05-21","08:30:00" )
 ];
 
 module.exports = { Venta, ventas };
