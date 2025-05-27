718d9ab4e0aac93a65ed5ddee75e4c3a2fd827a9
aleeM42 <mamarin.22@est.ucab.edu.ve>
2025-05-27 09:24:38 -0400

    pry complt

--- backend/routes/ventaRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ventaController = require("../controllers/consultarVentaController");
-
-router.get("/", ventaController.getVentas);
-
-module.exports = router;
